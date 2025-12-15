aaf60e8ab456dcd8ebd82e23058f4098a178cd35
Lanzhijiang <lanzhijiang@foxmail.com>
2025-12-15 21:16:36 +0800

    fix(inkLoading): flash weried

--- packages/web-design/src/components/inkLoading/inkLoading.story.vue
+++ packages/web-design/src/components/inkLoading/inkLoading.story.vue
@@ -3,12 +3,9 @@ import InkLoading from "./inkLoading.vue";
 </script>
 
 <template>
-  <Story
-    title="Feedback/Loading/[Semantic] Default"
-    :layout="{ type: 'single', iframe: false }"
-  >
-    <Variant title="Loading Indicator">
-      <div style="padding: 40px; display: flex; justify-content: center;">
+  <Story title="Feedback/Loading" :layout="{ type: 'single', iframe: false }">
+    <Variant title="Basic">
+      <div style="padding: 40px; display: flex; justify-content: center">
         <InkLoading />
       </div>
     </Variant>
